3dc0ce6af592d7f296b4066dcaa9db718365810d
Ayim William <odhisayim@gmail.com>
2026-01-05 19:39:06 +0300

    feat:finished navbar component inclusive of css

--- StelveyLogsitics/src/components/Navbar/Navbar.jsx
+++ StelveyLogsitics/src/components/Navbar/Navbar.jsx
@@ -4,15 +4,17 @@ import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <nav>
-        <img src={logo} alt="stelvey_logo" />
+    <nav className='container'>
+        <img src={logo} alt="stelvey_logo" className='logo' />
         <ul>
           <li>Home</li>
           <li>Services</li>
           <li>About Us</li>
           <li>Why Stelvey</li>
           <li>Testimonials</li>
-          <li>Contact Us</li>
+          <li>
+            <button className='btn'>Contact Us</button>
+          </li>
         </ul>
     </nav>
   )
