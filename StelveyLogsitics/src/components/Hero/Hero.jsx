1c469e5fe1f7f2ac4eba371c738a83faa88bcc8e
Ayim William <odhisayim@gmail.com>
2026-01-08 23:06:13 +0300

    feat: added the hero text and its css

--- StelveyLogsitics/src/components/Hero/Hero.jsx
+++ StelveyLogsitics/src/components/Hero/Hero.jsx
@@ -3,7 +3,18 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className='hero container'>
+      <div className="hero-text">
+        <h1>
+          We deliver reliable logistics for a connected world.
+        </h1>
+        <p>
+            Stelvey Logistics Limited provides efficient, secure and timely
+            transport solutions, ensuring your goods move seamlessly from origin to
+            destination with precision and care.
+        </p>
+        <button className='btn'>Explore More</button>
+      </div>
 
     </div>
   )
