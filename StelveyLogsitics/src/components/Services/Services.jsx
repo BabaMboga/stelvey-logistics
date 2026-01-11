1c18f4ac128da1dc47d423daaf679e18e722e8b8
Ayim William <odhisayim@gmail.com>
2026-01-11 14:47:31 +0300

    fix:replaced icons with something visible

--- StelveyLogsitics/src/components/Services/Services.jsx
+++ StelveyLogsitics/src/components/Services/Services.jsx
@@ -3,9 +3,9 @@ import './Services.css'
 import service_1 from '../../assets/service_1.jpg'
 import service_2 from '../../assets/service_2.jpg'
 import service_3 from '../../assets/service_3.jpg'
-import service_icon_1 from '../../assets/icons8-water-transportation-100.png'
-import service_icon_2 from '../../assets/icons8-warehouse-100.png'
-import service_icon_3 from '../../assets/icons8-customs-officer-100.png'
+import service_icon_1 from '../../assets/water-transportation-100.png'
+import service_icon_2 from '../../assets/warehouse-100.png'
+import service_icon_3 from '../../assets/customs-officer-100.png'
 
 const Services = () => {
   return (
