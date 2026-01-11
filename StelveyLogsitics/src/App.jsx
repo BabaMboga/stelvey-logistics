5157408f9023b37be76171bfb0f147fd89bc1147
Ayim William <odhisayim@gmail.com>
2026-01-11 16:34:02 +0300

    feat:added title page

--- StelveyLogsitics/src/App.jsx
+++ StelveyLogsitics/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Services from './components/Services/Services'
+import Title from './components/Title/Title'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
 
       {/*  adds some spacing on the sides */}
       <div className="container">
+        <Title/>
         <Services/>
       </div>
 
