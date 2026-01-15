7a926989b8ff4a87325cc6ea2c840ae134cb77f6
Ayim William <odhisayim@gmail.com>
2026-01-15 06:34:10 +0300

    fixed offset for linking to components

--- StelveyLogsitics/src/components/Navbar/Navbar.jsx
+++ StelveyLogsitics/src/components/Navbar/Navbar.jsx
@@ -18,12 +18,12 @@ const Navbar = () => {
         <img src={logo} alt="stelvey_logo" className='logo' />
         <ul>
           <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link> </li>
-          <li><Link to='service' smooth={true} offset={0} duration={500}>Services</Link> </li>
-          <li><Link to='about' smooth={true} offset={0} duration={500}>About Us</Link> </li>
-          <li><Link to='whystelvey' smooth={true} offset={0} duration={500}>Why Stelvey</Link> </li>
-          <li><Link to='testimonials' smooth={true} offset={0} duration={500}>Testimonials</Link> </li>
+          <li><Link to='service' smooth={true} offset={-260} duration={500}>Services</Link> </li>
+          <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link> </li>
+          <li><Link to='whystelvey' smooth={true} offset={-260} duration={500}>Why Stelvey</Link> </li>
+          <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link> </li>
           <li>
-            <Link to='contact' smooth={true} offset={0} duration={500} className='btn'>Contact Us</Link>
+            <Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link>
           </li>
         </ul>
     </nav>
