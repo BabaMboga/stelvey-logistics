b93644fe3c6565e9dfb1f0725d719da3d16a041c
Ayim William <odhisayim@gmail.com>
2026-01-15 07:38:01 +0300

    feat: added hidden Menu on mobile

--- StelveyLogsitics/src/components/Navbar/Navbar.jsx
+++ StelveyLogsitics/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-scroll'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import menu_icon from '../../assets/menu-icon.png'
 
 const Navbar = () => {
 
@@ -13,10 +14,16 @@ const Navbar = () => {
       window.scrollY > 650 ? setSticky(true) : setSticky(false);
     })
   }, []);
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+
+  }
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
         <img src={logo} alt="stelvey_logo" className='logo' />
-        <ul>
+        <ul className={mobileMenu ? '' : 'mobile-menu-hidden' }>
           <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link> </li>
           <li><Link to='service' smooth={true} offset={-260} duration={500}>Services</Link> </li>
           <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link> </li>
@@ -26,6 +33,7 @@ const Navbar = () => {
             <Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link>
           </li>
         </ul>
+        <img src={menu_icon} alt="menu_icon" className='menu-icon' onClick={toggleMenu} />
     </nav>
   )
 }
